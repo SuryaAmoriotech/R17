e56976d1f2ecb5bc5a1732627a7df2ce67a25e5e
Ajith-RRR <Ajith@amoriotech.com>
2023-06-22 16:31:25 +0530

    ajith

---
+++ application/logs/log-2023-06-22.php
@@ -0,0 +1,128 @@
+ERROR - 2023-06-22 11:00:12 --> $config['composer_autoload'] is set to TRUE but D:\new_wamp\www\R17\application\vendor/autoload.php was not found.
+INFO - 2023-06-22 11:00:12 --> Config Class Initialized
+INFO - 2023-06-22 11:00:12 --> Hooks Class Initialized
+DEBUG - 2023-06-22 11:00:12 --> UTF-8 Support Enabled
+INFO - 2023-06-22 11:00:12 --> Utf8 Class Initialized
+INFO - 2023-06-22 11:00:12 --> URI Class Initialized
+INFO - 2023-06-22 11:00:12 --> Router Class Initialized
+INFO - 2023-06-22 11:00:12 --> Output Class Initialized
+INFO - 2023-06-22 11:00:12 --> Security Class Initialized
+DEBUG - 2023-06-22 11:00:12 --> Global POST, GET and COOKIE data sanitized
+INFO - 2023-06-22 11:00:12 --> CSRF cookie sent
+INFO - 2023-06-22 11:00:12 --> Input Class Initialized
+INFO - 2023-06-22 11:00:12 --> Language Class Initialized
+INFO - 2023-06-22 11:00:12 --> Loader Class Initialized
+INFO - 2023-06-22 11:00:12 --> Helper loaded: url_helper
+INFO - 2023-06-22 11:00:12 --> Helper loaded: file_helper
+INFO - 2023-06-22 11:00:12 --> Helper loaded: form_helper
+INFO - 2023-06-22 11:00:12 --> Helper loaded: lang_helper
+INFO - 2023-06-22 11:00:12 --> Database Driver Class Initialized
+INFO - 2023-06-22 11:00:12 --> Session: Class initialized using 'files' driver.
+INFO - 2023-06-22 11:00:12 --> Parser Class Initialized
+INFO - 2023-06-22 11:00:12 --> Language file loaded: language/english/pagination_lang.php
+INFO - 2023-06-22 11:00:12 --> Pagination Class Initialized
+INFO - 2023-06-22 11:00:12 --> Form Validation Class Initialized
+INFO - 2023-06-22 11:00:12 --> Controller Class Initialized
+DEBUG - 2023-06-22 11:00:12 --> Auth class already loaded. Second attempt ignored.
+DEBUG - 2023-06-22 11:00:12 --> Session class already loaded. Second attempt ignored.
+INFO - 2023-06-22 11:00:12 --> Model "Hrm_model" initialized
+INFO - 2023-06-22 11:00:12 --> File loaded: D:\new_wamp\www\R17\application\views\hr/add_timesheet.php
+DEBUG - 2023-06-22 11:00:12 --> Parser class already loaded. Second attempt ignored.
+INFO - 2023-06-22 11:00:12 --> File loaded: D:\new_wamp\www\R17\application\views\include/top_menu.php
+INFO - 2023-06-22 11:00:12 --> File loaded: D:\new_wamp\www\R17\application\views\include/admin_loggedin_info.php
+INFO - 2023-06-22 11:00:12 --> Model "Products" initialized
+INFO - 2023-06-22 11:00:12 --> Model "Web_settings" initialized
+INFO - 2023-06-22 12:00:12 --> Model "reports" initialized
+INFO - 2023-06-22 12:00:12 --> Model "Users" initialized
+INFO - 2023-06-22 12:00:13 --> File loaded: D:\new_wamp\www\R17\application\views\include/admin_header.php
+ERROR - 2023-06-22 12:00:13 --> Severity: Notice --> Undefined variable: email_setting D:\new_wamp\www\R17\application\views\include\admin_footer.php 108
+ERROR - 2023-06-22 12:00:13 --> Severity: Notice --> Undefined variable: email_setting D:\new_wamp\www\R17\application\views\include\admin_footer.php 120
+ERROR - 2023-06-22 12:00:13 --> Severity: Notice --> Undefined variable: email_setting D:\new_wamp\www\R17\application\views\include\admin_footer.php 123
+INFO - 2023-06-22 12:00:13 --> File loaded: D:\new_wamp\www\R17\application\views\include/admin_footer.php
+INFO - 2023-06-22 12:00:13 --> File loaded: D:\new_wamp\www\R17\application\views\admin_html_template.php
+INFO - 2023-06-22 12:00:13 --> Final output sent to browser
+DEBUG - 2023-06-22 12:00:13 --> Total execution time: 1.1553
+ERROR - 2023-06-22 11:00:13 --> $config['composer_autoload'] is set to TRUE but D:\new_wamp\www\R17\application\vendor/autoload.php was not found.
+INFO - 2023-06-22 11:00:13 --> Config Class Initialized
+INFO - 2023-06-22 11:00:13 --> Hooks Class Initialized
+DEBUG - 2023-06-22 11:00:13 --> UTF-8 Support Enabled
+INFO - 2023-06-22 11:00:13 --> Utf8 Class Initialized
+INFO - 2023-06-22 11:00:13 --> URI Class Initialized
+INFO - 2023-06-22 11:00:13 --> Router Class Initialized
+ERROR - 2023-06-22 11:00:13 --> $config['composer_autoload'] is set to TRUE but D:\new_wamp\www\R17\application\vendor/autoload.php was not found.
+INFO - 2023-06-22 11:00:13 --> Output Class Initialized
+INFO - 2023-06-22 11:00:13 --> Security Class Initialized
+INFO - 2023-06-22 11:00:13 --> Config Class Initialized
+INFO - 2023-06-22 11:00:13 --> Hooks Class Initialized
+DEBUG - 2023-06-22 11:00:13 --> Global POST, GET and COOKIE data sanitized
+INFO - 2023-06-22 11:00:13 --> CSRF cookie sent
+INFO - 2023-06-22 11:00:13 --> Input Class Initialized
+DEBUG - 2023-06-22 11:00:13 --> UTF-8 Support Enabled
+INFO - 2023-06-22 11:00:13 --> Language Class Initialized
+INFO - 2023-06-22 11:00:13 --> Utf8 Class Initialized
+ERROR - 2023-06-22 11:00:13 --> 404 Page Not Found: Assets/css
+INFO - 2023-06-22 11:00:13 --> URI Class Initialized
+INFO - 2023-06-22 11:00:13 --> Router Class Initialized
+INFO - 2023-06-22 11:00:13 --> Output Class Initialized
+INFO - 2023-06-22 11:00:13 --> Security Class Initialized
+DEBUG - 2023-06-22 11:00:13 --> Global POST, GET and COOKIE data sanitized
+INFO - 2023-06-22 11:00:13 --> CSRF cookie sent
+INFO - 2023-06-22 11:00:13 --> Input Class Initialized
+INFO - 2023-06-22 11:00:13 --> Language Class Initialized
+ERROR - 2023-06-22 11:00:13 --> 404 Page Not Found: Assets/datatables
+ERROR - 2023-06-22 11:00:13 --> $config['composer_autoload'] is set to TRUE but D:\new_wamp\www\R17\application\vendor/autoload.php was not found.
+INFO - 2023-06-22 11:00:13 --> Config Class Initialized
+INFO - 2023-06-22 11:00:13 --> Hooks Class Initialized
+DEBUG - 2023-06-22 11:00:13 --> UTF-8 Support Enabled
+INFO - 2023-06-22 11:00:13 --> Utf8 Class Initialized
+INFO - 2023-06-22 11:00:13 --> URI Class Initialized
+INFO - 2023-06-22 11:00:13 --> Router Class Initialized
+INFO - 2023-06-22 11:00:13 --> Output Class Initialized
+INFO - 2023-06-22 11:00:13 --> Security Class Initialized
+DEBUG - 2023-06-22 11:00:13 --> Global POST, GET and COOKIE data sanitized
+INFO - 2023-06-22 11:00:13 --> CSRF cookie sent
+INFO - 2023-06-22 11:00:13 --> Input Class Initialized
+INFO - 2023-06-22 11:00:13 --> Language Class Initialized
+ERROR - 2023-06-22 11:00:13 --> 404 Page Not Found: Assets/js
+ERROR - 2023-06-22 11:00:14 --> $config['composer_autoload'] is set to TRUE but D:\new_wamp\www\R17\application\vendor/autoload.php was not found.
+INFO - 2023-06-22 11:00:14 --> Config Class Initialized
+INFO - 2023-06-22 11:00:14 --> Hooks Class Initialized
+DEBUG - 2023-06-22 11:00:14 --> UTF-8 Support Enabled
+INFO - 2023-06-22 11:00:14 --> Utf8 Class Initialized
+INFO - 2023-06-22 11:00:14 --> URI Class Initialized
+INFO - 2023-06-22 11:00:14 --> Router Class Initialized
+INFO - 2023-06-22 11:00:14 --> Output Class Initialized
+INFO - 2023-06-22 11:00:14 --> Security Class Initialized
+DEBUG - 2023-06-22 11:00:14 --> Global POST, GET and COOKIE data sanitized
+INFO - 2023-06-22 11:00:14 --> CSRF cookie sent
+INFO - 2023-06-22 11:00:14 --> Input Class Initialized
+INFO - 2023-06-22 11:00:14 --> Language Class Initialized
+ERROR - 2023-06-22 11:00:14 --> 404 Page Not Found: Assets/datatables
+ERROR - 2023-06-22 11:00:14 --> $config['composer_autoload'] is set to TRUE but D:\new_wamp\www\R17\application\vendor/autoload.php was not found.
+INFO - 2023-06-22 11:00:14 --> Config Class Initialized
+INFO - 2023-06-22 11:00:14 --> Hooks Class Initialized
+DEBUG - 2023-06-22 11:00:14 --> UTF-8 Support Enabled
+INFO - 2023-06-22 11:00:14 --> Utf8 Class Initialized
+INFO - 2023-06-22 11:00:14 --> URI Class Initialized
+INFO - 2023-06-22 11:00:14 --> Router Class Initialized
+INFO - 2023-06-22 11:00:14 --> Output Class Initialized
+INFO - 2023-06-22 11:00:14 --> Security Class Initialized
+DEBUG - 2023-06-22 11:00:14 --> Global POST, GET and COOKIE data sanitized
+ERROR - 2023-06-22 11:00:14 --> $config['composer_autoload'] is set to TRUE but D:\new_wamp\www\R17\application\vendor/autoload.php was not found.
+INFO - 2023-06-22 11:00:14 --> CSRF cookie sent
+INFO - 2023-06-22 11:00:14 --> Input Class Initialized
+INFO - 2023-06-22 11:00:14 --> Language Class Initialized
+INFO - 2023-06-22 11:00:14 --> Config Class Initialized
+INFO - 2023-06-22 11:00:14 --> Hooks Class Initialized
+ERROR - 2023-06-22 11:00:14 --> 404 Page Not Found: Assets/js
+DEBUG - 2023-06-22 11:00:14 --> UTF-8 Support Enabled
+INFO - 2023-06-22 11:00:14 --> Utf8 Class Initialized
+INFO - 2023-06-22 11:00:14 --> URI Class Initialized
+INFO - 2023-06-22 11:00:14 --> Router Class Initialized
+INFO - 2023-06-22 11:00:14 --> Output Class Initialized
+INFO - 2023-06-22 11:00:14 --> Security Class Initialized
+DEBUG - 2023-06-22 11:00:14 --> Global POST, GET and COOKIE data sanitized
+INFO - 2023-06-22 11:00:14 --> CSRF cookie sent
+INFO - 2023-06-22 11:00:14 --> Input Class Initialized
+INFO - 2023-06-22 11:00:14 --> Language Class Initialized
+ERROR - 2023-06-22 11:00:14 --> 404 Page Not Found: Chrm/my-assets
