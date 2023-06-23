0baaf06c51a2e1f7879f33531cba2a79749db96a
amoriotechnology <madhu@amoriotech.com>
2023-06-23 19:55:25 +0530

    surya_23

---
+++ application/logs/log-2023-06-23.php
@@ -0,0 +1,58 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2023-06-23 14:24:34 --> $config['composer_autoload'] is set to TRUE but D:\wamp\www\R17\application\vendor/autoload.php was not found.
+INFO - 2023-06-23 14:24:34 --> Config Class Initialized
+INFO - 2023-06-23 14:24:34 --> Hooks Class Initialized
+DEBUG - 2023-06-23 14:24:34 --> UTF-8 Support Enabled
+INFO - 2023-06-23 14:24:34 --> Utf8 Class Initialized
+INFO - 2023-06-23 14:24:34 --> URI Class Initialized
+INFO - 2023-06-23 14:24:34 --> Router Class Initialized
+INFO - 2023-06-23 14:24:34 --> Output Class Initialized
+INFO - 2023-06-23 14:24:34 --> Security Class Initialized
+DEBUG - 2023-06-23 14:24:34 --> Global POST, GET and COOKIE data sanitized
+INFO - 2023-06-23 14:24:34 --> CSRF cookie sent
+INFO - 2023-06-23 14:24:34 --> Input Class Initialized
+INFO - 2023-06-23 14:24:34 --> Language Class Initialized
+ERROR - 2023-06-23 14:24:34 --> 404 Page Not Found: Chrm/manage_timesheet
+ERROR - 2023-06-23 14:24:36 --> $config['composer_autoload'] is set to TRUE but D:\wamp\www\R17\application\vendor/autoload.php was not found.
+INFO - 2023-06-23 14:24:36 --> Config Class Initialized
+INFO - 2023-06-23 14:24:36 --> Hooks Class Initialized
+DEBUG - 2023-06-23 14:24:36 --> UTF-8 Support Enabled
+INFO - 2023-06-23 14:24:36 --> Utf8 Class Initialized
+INFO - 2023-06-23 14:24:36 --> URI Class Initialized
+INFO - 2023-06-23 14:24:36 --> Router Class Initialized
+INFO - 2023-06-23 14:24:36 --> Output Class Initialized
+INFO - 2023-06-23 14:24:36 --> Security Class Initialized
+DEBUG - 2023-06-23 14:24:36 --> Global POST, GET and COOKIE data sanitized
+INFO - 2023-06-23 14:24:36 --> CSRF cookie sent
+INFO - 2023-06-23 14:24:36 --> Input Class Initialized
+INFO - 2023-06-23 14:24:36 --> Language Class Initialized
+ERROR - 2023-06-23 14:24:36 --> 404 Page Not Found: Chrm/pay_slip_list
+ERROR - 2023-06-23 14:24:37 --> $config['composer_autoload'] is set to TRUE but D:\wamp\www\R17\application\vendor/autoload.php was not found.
+INFO - 2023-06-23 14:24:37 --> Config Class Initialized
+INFO - 2023-06-23 14:24:37 --> Hooks Class Initialized
+DEBUG - 2023-06-23 14:24:37 --> UTF-8 Support Enabled
+INFO - 2023-06-23 14:24:37 --> Utf8 Class Initialized
+INFO - 2023-06-23 14:24:37 --> URI Class Initialized
+INFO - 2023-06-23 14:24:37 --> Router Class Initialized
+INFO - 2023-06-23 14:24:37 --> Output Class Initialized
+INFO - 2023-06-23 14:24:37 --> Security Class Initialized
+DEBUG - 2023-06-23 14:24:37 --> Global POST, GET and COOKIE data sanitized
+INFO - 2023-06-23 14:24:37 --> CSRF cookie sent
+INFO - 2023-06-23 14:24:37 --> Input Class Initialized
+INFO - 2023-06-23 14:24:37 --> Language Class Initialized
+ERROR - 2023-06-23 14:24:37 --> 404 Page Not Found: Chrm/manage_timesheet
+ERROR - 2023-06-23 14:24:39 --> $config['composer_autoload'] is set to TRUE but D:\wamp\www\R17\application\vendor/autoload.php was not found.
+INFO - 2023-06-23 14:24:39 --> Config Class Initialized
+INFO - 2023-06-23 14:24:39 --> Hooks Class Initialized
+DEBUG - 2023-06-23 14:24:39 --> UTF-8 Support Enabled
+INFO - 2023-06-23 14:24:39 --> Utf8 Class Initialized
+INFO - 2023-06-23 14:24:39 --> URI Class Initialized
+INFO - 2023-06-23 14:24:39 --> Router Class Initialized
+INFO - 2023-06-23 14:24:39 --> Output Class Initialized
+INFO - 2023-06-23 14:24:39 --> Security Class Initialized
+DEBUG - 2023-06-23 14:24:39 --> Global POST, GET and COOKIE data sanitized
+INFO - 2023-06-23 14:24:39 --> CSRF cookie sent
+INFO - 2023-06-23 14:24:39 --> Input Class Initialized
+INFO - 2023-06-23 14:24:39 --> Language Class Initialized
+ERROR - 2023-06-23 14:24:39 --> 404 Page Not Found: Chrm/edit_timesheet
